616fa3a88f7232e6b85379c05530b8821cbf2089
Joao Pedro <blvck.joao37@gmail.com>
2025-11-15 22:05:02 -0300

    Feature - Adicionado arquivo do python :)

---
+++ serial.py
@@ -0,0 +1,12 @@
+"""
+Esse codigo devera interligar o arduino com o 
+codigo do python, o qual poderemos plotar graficos
+e fazer mais coisas interessantes
+
+"""
+
+import pyserial as ps
+import matplotlib as plt
+
+if __name__ == "__main__":
+    print("Yep, provisorio")
